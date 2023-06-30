af52e40b0fab588c2d92f2d89d08ca773d4061b0
Danieer Galvez <Moldycord@gmail.com>
2023-06-30 17:38:32 -0500

    Added some styles

--- app/src/main/java/com/danieer/galvez/pokeapp/HomeActivity.kt
+++ app/src/main/java/com/danieer/galvez/pokeapp/HomeActivity.kt
@@ -1,7 +1,6 @@
 package com.danieer.galvez.pokeapp
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import com.danieer.galvez.pokeapp.databinding.HomeActivityBinding
 
@@ -9,12 +8,11 @@ class HomeActivity : ComponentActivity() {
 
     private lateinit var homeBinding: HomeActivityBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         homeBinding = HomeActivityBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
-
     }
 
 }

af64844e62e15752c0295f6a5e3042e148bd7759
Danieer Galvez <Moldycord@gmail.com>
2023-06-30 16:48:13 -0500

    Add constraint layout home

--- app/src/main/java/com/danieer/galvez/pokeapp/HomeActivity.kt
+++ app/src/main/java/com/danieer/galvez/pokeapp/HomeActivity.kt
@@ -1,8 +1,20 @@
 package com.danieer.galvez.pokeapp
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
+import com.danieer.galvez.pokeapp.databinding.HomeActivityBinding
 
 class HomeActivity : ComponentActivity() {
 
+    private lateinit var homeBinding: HomeActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        homeBinding = HomeActivityBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+
+    }
 
 }
